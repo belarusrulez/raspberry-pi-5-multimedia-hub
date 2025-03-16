0d505fc91c6c827247ea55cbe6ce04d06f57e91f
belarusrulez <ivan@fedarau.com>
2025-03-16 12:40:42 -0700

    wireguard in progress

--- qbittorrent/sh/start.sh
+++ qbittorrent/sh/start.sh
@@ -1,29 +1,28 @@
 #!/bin/sh
+set -e
 
-# Bring up the WireGuard interface
+echo "Bringing up the WireGuard interface..."
 wg-quick up wg0
 
-# Enable IP forwarding
-sysctl -w net.ipv4.ip_forward=1
-
-# Set up NAT (Network Address Translation) for the WireGuard interface
+echo "Setting up NAT (Network Address Translation) for the WireGuard interface..."
 iptables -t nat -A POSTROUTING -o wg0 -j MASQUERADE
 
-# Get the public IP address
-IP=$(curl -s https://ipinfo.io/ip)
-echo "Public IP: $IP"
+echo "Allowing traffic forwarding through the WireGuard interface..."
+iptables -A FORWARD -i wg0 -j ACCEPT
+iptables -A FORWARD -o wg0 -j ACCEPT
 
-# Get the country code for the IP address
-COUNTRY=$(curl -s https://ipinfo.io/$IP/country)
-echo "Country: $COUNTRY"
+echo "Routing all traffic through the WireGuard interface..."
+ip route add default dev wg0
 
+echo "Getting the country code for the IP address..."
+COUNTRY=$(curl --interface wg0 -s https://ipinfo.io/country)
+
+echo "Country code: $COUNTRY"
 # Check if the country code is RO (Romania)
 if [ "$COUNTRY" = "RO" ]; then
-  echo "Connected to WireGuard and IP is from Romania (RO)"
-else
-  echo "Not connected to WireGuard or IP is not from Romania (RO)"
+  echo "Country is Romania. Exiting..."
   exit 1
 fi
 
-# Start qBittorrent
+echo "Starting qBittorrent..."
 qbittorrent-nox --webui-port=8080
