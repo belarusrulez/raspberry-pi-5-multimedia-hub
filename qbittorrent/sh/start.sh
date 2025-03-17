26e9136043a3e2c4e4cc3b4a802e35cf03ebc8dd
belarusrulezmini <belarusrulezmini@belarusrulezmini.belarusrulezmini>
2025-03-16 22:06:38 -0700

    works

--- qbittorrent/sh/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-set -e
-
-echo "Starting qBittorrent..."
-qbittorrent-nox --webui-port=8080
--- test.sh
+++ test.sh
@@ -1,2 +1,5 @@
 curl --interface wg0 -s https://ipinfo.io/country
 curl -s https://ipinfo.io/country
+
+iptables -L -v -n
+tcpdump -ni wg0 port 8080
