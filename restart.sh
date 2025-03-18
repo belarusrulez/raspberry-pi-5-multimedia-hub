2ebdf1ea4ac88814685584d866dc96e557fe8f69
belarusrulezmini <belarusrulezmini@belarusrulezmini.belarusrulezmini>
2025-03-18 10:09:07 -0700

    removed jellyfin

--- restart.sh
+++ restart.sh
@@ -5,15 +5,15 @@ set -e
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 echo "Script directory: $SCRIPT_DIR"
 
+docker-compose down
 colima stop
+#colima delete
 
 # Create necessary directories
 mkdir -p ${SCRIPT_DIR}/mount/wireguard
 mkdir -p ${SCRIPT_DIR}/mount/downloads
 mkdir -p ${SCRIPT_DIR}/mount/qBittorrent/config
-mkdir -p ${SCRIPT_DIR}/mount/jellyfin/config
-mkdir -p ${SCRIPT_DIR}/mount/jellyfin/cache
 chmod -R 755 ${SCRIPT_DIR}/mount
 
-colima start --cpu 8 --memory 14 --mount /Volumes/2TB/media:/media:w --mount ${SCRIPT_DIR}/mount:/mount:w
+colima start --mount /Volumes/2TB/media:/media:w --mount ${SCRIPT_DIR}/mount:/mount:w
 docker-compose up -d
