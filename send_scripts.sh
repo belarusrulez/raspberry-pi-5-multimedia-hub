43ac9368a03613f8ef526117c44f2e969201d84a
belarusrulez <ivan@fedarau.com>
2025-03-21 20:34:32 -0700

    setup_q_bittorrent.sh

--- send_scripts.sh
+++ send_scripts.sh
@@ -4,4 +4,4 @@ set -e
 
 LOCAL_DIR="$(cd "$(dirname "$0")" && pwd)"
 
-rsync -av --delete $LOCAL_DIR/ubuntu_scripts belarusrulez@192.168.0.43:/home/belarusrulez/ubuntu_scripts
+rsync -av --delete $LOCAL_DIR/ubuntu_scripts belarusrulez@192.168.0.43:/home/belarusrulez
---
+++ ubuntu_scripts/setup_q_bittorrent.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -x
+set -e
+
+LOCAL_DIR="$(cd "$(dirname "$0")" && pwd)"
+# Update the system and install qBittorrent-nox
+sudo apt-get update -y
+sudo apt-get install -y qbittorrent-nox
+
+sudo systemctl stop qbittorrent-nox
+# Create qBittorrent.conf systemd service file for qBittorrent-nox
+sudo mkdir -p /home/belarusrulez/.config/qBittorrent
+cp -f $LOCAL_DIR/files/qBittorrent/qBittorrent.conf /home/belarusrulez/.config/qBittorrent/qBittorrent.conf
+
+# Enable and start the qBittorrent-nox service
+sudo systemctl daemon-reload
+sudo systemctl enable qbittorrent-nox
+sudo systemctl start qbittorrent-nox
+
+# Verify the service status
+sudo systemctl status qbittorrent-nox
+
+echo "qBittorrent-nox installation and configuration completed."
