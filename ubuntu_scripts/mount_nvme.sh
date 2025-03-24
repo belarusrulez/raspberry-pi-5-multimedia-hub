5f08d490b2efe930d32f6234674e876cbfcfce13
belarusrulez <ivan@fedarau.com>
2025-03-23 20:58:13 -0700

    mount_nvme.sh

--- ubuntu_scripts/mount_nvme.sh
+++ ubuntu_scripts/mount_nvme.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 set -x
+
 #sudo apt update
 #sudo apt install exfatprogs -y
 #sudo mkfs.exfat /dev/nvme0n1
@@ -8,7 +9,6 @@ set -x
 #sudo mount -t exfat -o umask=0000 /dev/nvme0n1 /mnt/nvme
 #echo 'UUID=FFE6-D58F /mnt/nvme exfat defaults,umask=0000 0 0' | sudo tee -a /etc/fstab
 #sudo reboot
-
 
 DEVICE=$(lsblk -o NAME,SIZE | grep '1.9T' | awk '{print $1}')
 sudo apt update
